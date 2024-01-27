293b542589a0d73958af4d5b71c7d7560c7a3828
Paulo Pereira <paulovvp@outlook.com>
2024-01-27 19:04:35 -0300

    falta pouco

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Header from './Header/Header';
 import Sidebar from './Sidebar/Sidebar';
+import Footer from './footer/footer';
+import Main from './main/main';
 <script src="https://kit.fontawesome.com/d6d5d5a885.js" crossorigin="anonymous"></script>
 
 function App() {
   return (
       <>
-      <Sidebar/>
       <Header/>
+      <Sidebar/>
+      <Main/>
+      <Footer/>
       </>
   );
 }
